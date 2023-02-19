bf60159ca6a06043c1220a7d70f88790f9d65468
juan75amieva <juan75amieva@gmail.com>
2023-02-19 01:04:25 +0100

    feat: create routes products

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express"
+import morgan from 'morgan'
+import pkg from '../package.json'
+
+import productRoutes from './routes/product.routes'
+
+const app = express()
+
+app.set('pkg',pkg)
+
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+    res.json({
+        nombre:app.get('pkg').name,
+        author: app.get('pkg').author,
+        description:app.get('pkg').description,
+        version:app.get('pkg').version
+    })
+})
+
+app.use('/product',productRoutes)
+
+export default app
+
+

0929715de369485f3ec0def801fc480f27f1c48b
juan75amieva <juan75amieva@gmail.com>
2023-02-20 23:11:57 +0100

    feat:add roles

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ import express from "express"
 import morgan from 'morgan'
 import pkg from '../package.json'
 
+
 import productRoutes from './routes/product.routes'
 import authRoutes from "./routes/auth.routes.js";
+import { createRoles } from "./libs/initialSetup";
 
 const app = express()
+createRoles()
 
 app.set('pkg',pkg)
 
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User"
+import Role from "../models/Role"
 import jwt from "jsonwebtoken";
 require('dotenv').config()
 
@@ -9,27 +10,39 @@ const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 export const signUp = async (req, res) => {
     const { username, email, password, roles } = req.body;
     console.log(req.body)
-    
+
     // Creating a new User Object
     const newUser = new User({
         username,
         email,
-        password: await User.encryptPassword(password) 
-        
+        password: await User.encryptPassword(password)
+
     });
 
+
+    // checking for roles
+    if (roles) {
+        const foundRoles = await Role.find({ name: { $in: roles } });
+        newUser.roles = foundRoles.map((role) => role._id);
+    } else {
+        const role = await Role.findOne({ name: "user" });
+        newUser.roles = [role._id];
+    }
+
+
     console.log(newUser)
-    
+
     const savedUser = await newUser.save()
-    
-
-     const token = jwt.sign({id: savedUser._id}, JWT_SECRET_KEY,{
-         expiresIn:86400
-     })
+    console.log(savedUser)
 
 
-    res.json({token})
-    
+    const token = jwt.sign({ id: savedUser._id }, JWT_SECRET_KEY, {
+        expiresIn: 86400
+    })
+
+
+    res.json({ token })
+
 }
 
 

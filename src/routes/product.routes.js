9cb046cda0b2d0cdcae02b0c375a3f75bb6a230a
juan75amieva <juan75amieva@gmail.com>
2023-02-21 09:28:02 +0100

    feat:add roles

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import {verifyToken} from "../middlewares/index"
+import {verifyToken, isAdmin, isModerator} from "../middlewares/index"
 const router = Router()
 
 import * as productsCtrl from '../controllers/product.controller'
 
-router.post('/',verifyToken ,productsCtrl.createProduct)
+router.post('/',verifyToken, isModerator, productsCtrl.createProduct)
 router.get('/', verifyToken,productsCtrl.getProducts)
-router.get('/:productId',verifyToken, productsCtrl.getProductById)
-router.put('/:productId',verifyToken, productsCtrl.updateProductById)
-router.delete('/:productId',verifyToken, productsCtrl.deleteProductById)
+router.get('/:productId',verifyToken,  productsCtrl.getProductById)
+router.put('/:productId',verifyToken, isAdmin, productsCtrl.updateProductById)
+router.delete('/:productId',verifyToken, isAdmin, productsCtrl.deleteProductById)
 
 export default router

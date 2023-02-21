9b8eb61ec1f196395711ca8da6beb6fad7094c55
juan75amieva <juan75amieva@gmail.com>
2023-02-21 12:14:51 +0100

    feat: verify usar and email exist

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import {createUser} from '../controllers/user.controllers'
+import {verifyToken, isAdmin} from '../middlewares/authJwt'
+import { checkExistingRole } from "../middlewares";
 const router = Router()
+
+
+router.post('/',verifyToken, isAdmin, checkExistingRole, createUser )
 
 export default router
